7e93cd36658b5c66d7aea023658ee94737323759
PADLEPOP <136889903+PADLEPOP@users.noreply.github.com>
2026-04-24 10:37:55 +0800

    Update text content in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Mr.Bus punya Sesuatu buat Kamu`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
