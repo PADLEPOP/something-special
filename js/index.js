65f4e31117db10ebfd3f366b2ef9540868551f93
PADLEPOP <136889903+PADLEPOP@users.noreply.github.com>
2026-04-24 10:42:07 +0800

    Fix text casing in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Mr.Bus punya Sesuatu buat Kamu`.split('')
+const text = `Mr.Bus Punya Sesuatu Buat kamu`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
